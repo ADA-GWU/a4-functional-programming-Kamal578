cd28b2dd1d66c6e4464a3573f9a604637bcf3d46
Ahmadov Kamal <k.ahmadov@caspiansoft.com>
2025-11-19 12:16:53 +0400

    Initial successful attempt

--- A4-currying/app/Main.hs
+++ A4-currying/app/Main.hs
@@ -1,6 +1,21 @@
-module Main (main) where
+-- app/Main.hs
 
-import Lib
+module Main (main) where
+import Network.HTTP.Conduit
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Char8 as B8
+
+-- Curried function to build the request
+restClient :: String -> String -> String -> IO L8.ByteString
+restClient httpMethod url endpoint = do
+    let fullUrl = url ++ endpoint
+    manager <- newManager tlsManagerSettings
+    req <- parseRequest fullUrl
+    let request = req { method = B8.pack httpMethod } 
+    response <- httpLbs request manager
+    return (responseBody response)
 
 main :: IO ()
-main = someFunc
+main = do
+    response <- restClient "GET" "https://jsonplaceholder.typicode.com" "/posts/1"
+    L8.putStrLn response
